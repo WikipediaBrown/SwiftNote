665a0899694c13fd4f69512843b8c9846e46d6e9
Perris Davis <perrisdavis@me.com>
2016-06-05 00:47:25 -0700

    Created MainViewController and assigned it to a UINavigationController and that in turn I assigned to the root view. Additionally I subclassed MainViewController from UITableViewController.

--- SwiftNote/AppDelegate.swift
+++ SwiftNote/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        
+        let controller = MainViewController()
+        let navigationController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = navigationController
+        
         // Override point for customization after application launch.
         return true
     }
