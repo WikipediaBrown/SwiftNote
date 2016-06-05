b13f7ffeceff525ee2c25364217a4819f7ac8ffa
Perris Davis <perrisdavis@me.com>
2016-06-05 00:01:59 -0700

    Initial Commit

---
+++ SwiftNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNote
+//
+//  Created by Perris Davis on 6/5/16.
+//  Copyright © 2016 Wikipedia Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
