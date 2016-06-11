f5ddab7b0f8178208267888cee774734615214ce
Perris Davis <perrisdavis@me.com>
2016-06-11 16:58:59 -0700

    Commented out some rudimentary auto layout constraints and added a dismiss button and dismiss functionality.

--- SwiftNote/MakeNoteViewController.swift
+++ SwiftNote/MakeNoteViewController.swift
@@ -12,8 +12,6 @@ class MakeNoteViewController: UIViewController, UINavigationBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         // Sets background color
         self.view.backgroundColor = UIColor.redColor()
@@ -28,33 +26,46 @@ class MakeNoteViewController: UIViewController, UINavigationBarDelegate {
         
         //Add UINavigation Bar
         let navBar: UINavigationBar = UINavigationBar(frame: CGRectMake(0, 0, self.view.frame.size.width, 64))
+        //navBar.translatesAutoresizingMaskIntoConstraints = false
         let navBarItem = UINavigationItem()
         navBar.items = [navBarItem]
         navBar.delegate = self
         self.view.addSubview(navBar)
         
-        navBar.backgroundColor = UIColor.whiteColor()
+        navBar.backgroundColor = UIColor.grayColor()
         
         //Add UINavigationItem Title and Prompt
-        navBarItem.title = ""
+        navBarItem.title = "Jot Down Note"
         navBarItem.prompt = ""
         
         //Add UINavigationItem Left Button ()
-        navBarItem.leftBarButtonItem = UIBarButtonItem(title: "Save", style:   UIBarButtonItemStyle.Plain, target: self, action: #selector(MakeNoteViewController.leftNavBarButton))
+        navBarItem.leftBarButtonItem = UIBarButtonItem(title: "", style:   UIBarButtonItemStyle.Plain, target: self, action: #selector(MakeNoteViewController.leftNavBarButton))
         
         //Add UINavigationItem Right Button
         navBarItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style:   UIBarButtonItemStyle.Plain, target: self, action: #selector(MakeNoteViewController.rightNavBarButton))
-
-
         
+        /*
+        //Dictionary of Views with just the navBar
+        let viewsDictionary = ["navBar": navBar]
+        
+        //Add navBar constraints
+        let navBarConstraintWidth = NSLayoutConstraint.constraintsWithVisualFormat("H:[navBar(44)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewsDictionary)
+        
+        let navBarConstraintHeight = NSLayoutConstraint.constraintsWithVisualFormat("V:[navBar(44)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewsDictionary)
+        
+        navBar.addConstraint(navBarConstraintWidth)
+        navBar.addConstraint(navBarConstraintHeight)
+        */
     }
     
+    //This method is called when the left navBar button is pressed.
     func leftNavBarButton() {
     
     }
     
+    //This method is called when the right navBar button is pressed.
     func rightNavBarButton() {
-    
+        self.dismissViewControllerAnimated(false, completion: nil)
     }
 
 }
