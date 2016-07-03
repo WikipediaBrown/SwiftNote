fd34c0b9abcad5f26c77029059dc0a36b63e4ae5
Perris Davis <perrisdavis@me.com>
2016-07-02 19:30:44 -0700

    Added more documentation.

--- SwiftNote/InitialAnimatedViewController.swift
+++ SwiftNote/InitialAnimatedViewController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 import IoniconsSwift
-import Hue
-
+// This is the animation for the initial s
+//------------------------------------------------------------
 class InitialAnimatedViewController: UIViewController {
     
+    // This calls the setupImage function and hides the navigation bar.
+    //------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
         setupImage()
-        
-        
         self.navigationController?.navigationBar.hidden = true
     }
+    //------------------------------------------------------------
     
-    
+    // This creates the image to be animated.
+    //------------------------------------------------------------
     var initialScreenLabel: UILabel = {
         var label = UILabel()
         label = Ionicons.Icecream.label(200)
@@ -31,13 +32,15 @@ class InitialAnimatedViewController: UIViewController {
         
         return label
     }()
+    //------------------------------------------------------------
     
+    // This sets up the image and its animation then pushes the main view controller onto the stack.
+    //------------------------------------------------------------
     func setupImage() {
-        
+
         self.view.addSubview(initialScreenLabel)
         let mainViewController = MainViewController()
         UIView.animateWithDuration(2, delay: 0, options: .CurveEaseIn, animations: {
-            
             self.initialScreenLabel.transform = CGAffineTransformMakeScale(2, 2)
             self.rotateView()
             self.rotateView()
@@ -66,10 +69,14 @@ class InitialAnimatedViewController: UIViewController {
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[initialScreenImage]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[initialScreenImage]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
+    //------------------------------------------------------------
     
+    // This funciton rotates the image 20% of a circle.
+    //------------------------------------------------------------
     func rotateView() {
     
         self.initialScreenLabel.transform = CGAffineTransformRotate(self.initialScreenLabel.transform, CGFloat((M_PI/5)))
     }
+    //------------------------------------------------------------
     
 }
